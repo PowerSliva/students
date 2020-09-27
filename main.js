ce4045b6804c6d86ba68fee1d97f4f45eaf4cdf3
PowerSliva <71924695+PowerSliva@users.noreply.github.com>
2020-09-27 20:33:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -52,3 +52,75 @@ function sortzad3(masszad3) {
     return console.log(masszad3);
 }
 sortzad3(masszad3)
+
+/**4. Написать функцию, которая 25 раз выведет в консоль: 
+ * «Я номер 1», «я номер 2», «Я номер 3» и так далее до 25. 
+ * */
+kol = prompt("Сколько повторений вы хотите");
+function Inam(kol) {
+  for (let i=1; i<=kol; i++) {
+       console.log(`Я номер ${i}`);
+      }
+}
+Inam(kol)
+/**
+ * Написать функцию, которая вернёт из исходного массива
+ *  [10,20,30,40,50,60,70] следующий массив [70,60,50,40,30,20,10]
+ */
+let masszad4 = [10,20,30,40,50,60,70];
+
+function otobmass(masszad4) {
+  masszad4.reverse();
+  return console.log(masszad4);
+}
+otobmass(masszad4) 
+/**
+ * 6. Написать функцию, которая будет принимать на вход имя, 
+ * а возвращать массив из букв его имени, то есть: 
+ * вход - «Саша», выход - [‘с’, ‘а’, ‘ш’, ‘а’]
+ */
+names = prompt("Введите имя");
+function rasbit(names) {
+ let rasname = names.split('');
+ return console.log(rasname);
+}
+rasbit(names)
+/**
+ * 7. Написать функцию, которая принимает на вход массив из 4 чисел,
+ *  и возвращает сумму этих числе. То есть, вход: [1,2,3,4], выход: 10
+*/
+let masszad7 = [1,2,3,4];
+ function massum(masszad7) {
+  result = masszad7.reduce((sum, current) => sum + current, 0);
+    return console.log(result);
+}
+massum(masszad7)
+
+/**
+ * 8. Написать функцию, которая принимает на вход строку, 
+ * и выводит в консоль её длину. 
+ * */
+/**
+let arr = prompt("")
+console.log(arr.lenght)
+Не понимаю, почему при вводе предложения через ,prompt, выходит неопределенность.
+Prompt ведь тоже преобразует введеные в него данные в текст? 
+ */
+let slovo = "Синяя пртица"
+function izdlina(slovo) {
+  return console.log(slovo.length);
+}
+izdlina(slovo)
+/**
+ * 9. Написать функцию, которая принимает на вход имя и число n,
+ *  и выводит в консоль заданное имя n-раз.
+ */
+let imy = prompt("Введите имя");
+let chislo = prompt("Введите число");
+function viviod9(imy, chislo) {
+  
+  for (let i=0; i<=chislo; i++) {
+    console.log(imy);
+   }
+ }
+viviod9(imy, chislo)
